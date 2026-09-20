1f8241d0bb06de2f0b40b8d13d2047a0a188c506
Rudra Saiprasad <saiprasadrudra10@gmail.com>
2026-09-20 22:04:32 +0530

    day 1

--- Day-1/findtheLongestWord.js
+++ Day-1/findtheLongestWord.js
@@ -1,19 +1,34 @@
 const str = "i am the king of the universe";
 
 const findtheLongestWord = (str) => {
-  if (str.lenght === 0) {
+  if (str.trim().length === 0) {
     return false;
   }
 
   const words = str.split(" ");
-  words.sort((a, b) => a.length - b.length);
-  console.log(words.at(-1));
 
   /*
-      or we can also write like this.
-    words.sort((a, b) => b.length - a.length);
-    console.log(words[0]);
+  method 1 
+
+  words.sort((a, b) => a.length - b.length);
+  return words.at(-1);
   */
+
+  /*
+  method 2
+    words.sort((a, b) => b.length - a.length);
+    return words[0];
+  */
+
+  /*
+    method 3 
+    
+    
+    */
+  const thatWord = words.reduce((acc, current) =>
+    current.length > acc.length ? current : acc,
+  );
+  return thatWord;
 };
 
-findtheLongestWord(str);
+console.log(findtheLongestWord(str));
